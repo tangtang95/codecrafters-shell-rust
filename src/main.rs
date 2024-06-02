9e5969226fa376c385925da9f4d2e165e6489f60
Tangtang Zhou <tangtang2995@gmail.com>
2024-06-02 18:19:17 +0200

    feat: implement type command

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,12 @@ fn main() {
                 Command::NoInput => println!(),
                 Command::Exit(exit_code) => std::process::exit(exit_code),
                 Command::Echo(text) => println!("{}", text),
+                Command::Type(cmd) => {
+                    match cmd.as_ref() {
+                        EXIT_CMD | ECHO_CMD | TYPE_CMD => println!("{} is a shell builtin", cmd),
+                        _ => println!("{} not found", cmd)
+                    }
+                },
             },
             Err(error) => println!("{}", error),
         }
@@ -25,11 +31,13 @@ fn main() {
 
 const EXIT_CMD: &str = "exit";
 const ECHO_CMD: &str = "echo";
+const TYPE_CMD: &str = "type";
 
 enum Command {
     NoInput,
     Exit(i32),
-    Echo(String)
+    Echo(String),
+    Type(String)
 }
 
 #[derive(Error, Debug)]
@@ -57,6 +65,10 @@ fn parse_command(input: &str) -> Result<Command, Error> {
             let echo_text = splitted_input.collect::<Vec<&str>>().join(" ");
             Ok(Command::Echo(echo_text.to_string()))
         },
+        Some(TYPE_CMD) => {
+            let command_name = splitted_input.collect::<Vec<&str>>().join(" ");
+            Ok(Command::Type(command_name))
+        }
         Some(command) => Err(Error::CommandNotFound(command.to_string())),
         None => Ok(Command::NoInput),
     }
