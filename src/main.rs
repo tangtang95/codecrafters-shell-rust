b53f4212a775b7dbc7aae0e810c62a9b5dcbdaa3
Tangtang Zhou <tangtang2995@gmail.com>
2024-06-02 17:35:47 +0200

    chore: implement easy repl

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim());
+        println!("{}: command not found", input.trim());
+    }
 }
