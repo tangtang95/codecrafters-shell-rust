6423f6d7ac7542425d16b7a888d60168352593fb
Tangtang Zhou <tangtang2995@gmail.com>
2024-06-02 18:39:39 +0200

    feat: improve type command with PATH env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::collections::HashMap;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 use thiserror::Error;
 
 fn main() {
+    let path_env = std::env::var("PATH").unwrap();
+    let path_entries = path_env.split(':').collect::<Vec<&str>>();
+    let mut command_map: HashMap<String, String> = HashMap::new();
+    for path_entry in path_entries {
+        let dir = std::fs::read_dir(path_entry);
+        if let Ok(dir) = dir {
+            for dir_entry in dir {
+                let dir_entry = dir_entry.unwrap();
+                let filename = dir_entry.file_name().into_string().unwrap();
+                command_map.insert(filename, path_entry.to_string());
+            }
+        }
+    }
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -17,11 +32,12 @@ fn main() {
                 Command::NoInput => println!(),
                 Command::Exit(exit_code) => std::process::exit(exit_code),
                 Command::Echo(text) => println!("{}", text),
-                Command::Type(cmd) => {
-                    match cmd.as_ref() {
-                        EXIT_CMD | ECHO_CMD | TYPE_CMD => println!("{} is a shell builtin", cmd),
-                        _ => println!("{} not found", cmd)
-                    }
+                Command::Type(cmd) => match cmd.as_ref() {
+                    EXIT_CMD | ECHO_CMD | TYPE_CMD => println!("{} is a shell builtin", cmd),
+                    cmd => match command_map.get(cmd) {
+                        Some(path_src) => println!("{} is {}/{}", cmd, path_src, cmd),
+                        None => println!("{} not found", cmd),
+                    },
                 },
             },
             Err(error) => println!("{}", error),
@@ -37,7 +53,7 @@ enum Command {
     NoInput,
     Exit(i32),
     Echo(String),
-    Type(String)
+    Type(String),
 }
 
 #[derive(Error, Debug)]
@@ -60,11 +76,11 @@ fn parse_command(input: &str) -> Result<Command, Error> {
                 Some(_) => Err(Error::ParseCommandError(EXIT_CMD.to_string())),
                 None => Ok(Command::Exit(exit_code)),
             }
-        },
+        }
         Some(ECHO_CMD) => {
             let echo_text = splitted_input.collect::<Vec<&str>>().join(" ");
             Ok(Command::Echo(echo_text.to_string()))
-        },
+        }
         Some(TYPE_CMD) => {
             let command_name = splitted_input.collect::<Vec<&str>>().join(" ");
             Ok(Command::Type(command_name))
