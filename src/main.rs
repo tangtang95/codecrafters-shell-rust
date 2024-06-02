a595428129f7c0bcd456ebbd32f3dbbeda4bac1f
Tangtang Zhou <tangtang2995@gmail.com>
2024-06-02 18:06:07 +0200

    feat: implement exit command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+use thiserror::Error;
+
 fn main() {
     loop {
         print!("$ ");
@@ -10,7 +12,45 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
+        match parse_command(&input) {
+            Ok(command) => match command {
+                Command::NoInput => println!(),
+                Command::Exit(exit_code) => std::process::exit(exit_code),
+            },
+            Err(error) => println!("{}", error),
+        }
+    }
+}
 
-        println!("{}: command not found", input.trim());
+const EXIT_CMD: &str = "exit";
+
+enum Command {
+    NoInput,
+    Exit(i32),
+}
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("{0}: command not found")]
+    CommandNotFound(String),
+    #[error("Error during parse of command {0}")]
+    ParseCommandError(String),
+}
+
+fn parse_command(input: &str) -> Result<Command, Error> {
+    let mut splitted_input = input.trim().split(' ');
+    match splitted_input.next() {
+        Some(EXIT_CMD) => {
+            let exit_code = splitted_input.next().unwrap_or("0");
+            let exit_code = exit_code
+                .parse::<i32>()
+                .map_err(|_| Error::ParseCommandError(EXIT_CMD.to_string()))?;
+            match splitted_input.next() {
+                Some(_) => Err(Error::ParseCommandError(EXIT_CMD.to_string())),
+                None => Ok(Command::Exit(exit_code)),
+            }
+        }
+        Some(command) => Err(Error::CommandNotFound(command.to_string())),
+        None => Ok(Command::NoInput),
     }
 }
